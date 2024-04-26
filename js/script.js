20fc2d212308041d7a3f5289c5e666118ffee3b1
t2023-m0069 <t2023-m0069@t2023-m0069s-MacBook-Air.local>
2024-04-26 14:05:22 +0900

    change language

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const options = {
 const movieCardBox = document.querySelector(".movieCardBox");
 console.log(movieCardBox);
 
-fetch('https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=1', options)
+fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
     .then(response => response.json())
     .then(response => {
         let results = response.results;
